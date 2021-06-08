5a12a43c6ef91d0504be6bcee954800a365a16fb
Ricard <ricardpinzonsuller@gmail.com>
2021-06-08 12:08:15 +0200

    -Layout design has been improved.
    -Created the first instance of the ExoPlayer.

--- app/src/main/java/ricard/projecte/exoplayer_technicaltest/MainActivity.java
+++ app/src/main/java/ricard/projecte/exoplayer_technicaltest/MainActivity.java
@@ -3,20 +3,37 @@ package ricard.projecte.exoplayer_technicaltest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ui.PlayerView;
 
 import java.util.SimpleTimeZone;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FrameLayout fl = findViewById(R.id.Flayout_player);
 
+
+        PlayerView playview = new PlayerView(this);
+        fl.addView(playview);
 
         SimpleExoPlayer player = new SimpleExoPlayer.Builder(this).build();
+
+        player.setPlayWhenReady(true);
+
+        playview.setPlayer(player);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
