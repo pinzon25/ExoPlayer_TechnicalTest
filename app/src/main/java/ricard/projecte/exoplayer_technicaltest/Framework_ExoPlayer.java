dffca9c5d18a6ebb7ea5cdc7d1e93d8e6e31baf7
Ricard <ricardpinzonsuller@gmail.com>
2021-06-08 15:24:59 +0200

    -Implemented functional ExoPlayer.
    -Attached MediaItem to Player.
    
    -Modified activityMain layout to ad counters textfields.

--- app/src/main/java/ricard/projecte/exoplayer_technicaltest/Framework_ExoPlayer.java
+++ app/src/main/java/ricard/projecte/exoplayer_technicaltest/Framework_ExoPlayer.java
@@ -1,4 +1,22 @@
 package ricard.projecte.exoplayer_technicaltest;
 
-public class Framework_ExoPlayer {
+import android.content.Context;
+import android.net.Uri;
+
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.upstream.DataSpec;
+import com.google.android.exoplayer2.upstream.FileDataSource;
+import com.google.android.exoplayer2.util.MimeTypes;
+import com.google.android.exoplayer2.util.Util;
+
+import java.io.File;
+
+public class Framework_ExoPlayer{
+
+    public static MediaItem getMediaItem(){
+        MediaItem mi = MediaItem.fromUri(Uri.parse("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4"));
+        return mi;
+    }
+
 }
--- app/src/main/java/ricard/projecte/exoplayer_technicaltest/MainActivity.java
+++ app/src/main/java/ricard/projecte/exoplayer_technicaltest/MainActivity.java
@@ -2,13 +2,21 @@ package ricard.projecte.exoplayer_technicaltest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
 
+import com.google.android.exoplayer2.MediaItem;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.RawResourceDataSource;
+import com.google.android.exoplayer2.util.MimeTypes;
 
+import java.io.File;
 import java.util.SimpleTimeZone;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -25,12 +33,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         SimpleExoPlayer player = new SimpleExoPlayer.Builder(this).build();
 
+        playview.setPlayer(player);
+
+        MediaItem mi = Framework_ExoPlayer.getMediaItem();
+
+        //player.setMediaItem(getMediaItem());
+
+        player.addMediaItem(mi);
+
+        player.prepare();
+
         player.setPlayWhenReady(true);
 
-        playview.setPlayer(player);
+
+
+
+
+
 
 
     }
+//C:\Users\riki\Desktop\CFGS DAM\ExoPlayerTechnicalTest\app\src\main\res\video
+
 
     @Override
     public void onClick(View v) {
