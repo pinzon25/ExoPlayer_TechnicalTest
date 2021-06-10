7b59f9b193a9567b62f7268a81316178123f78f2
Ricard <ricardpinzonsuller@gmail.com>
2021-06-10 15:47:53 +0200

    -Implemented customized PlayerControl.
    -Implemented button listeners to buttons of play, pause and repeat.
    
    -Added click counter metods of the play/pause,restart buttons and added a method to calculate the elapsed time between pause and play/resume clicks into the Framework_ExoPlayer class.
    
    -Implemented Toasts in the first play and when the first frame is displayed.

--- app/src/main/java/ricard/projecte/exoplayer_technicaltest/Framework_ExoPlayer.java
+++ app/src/main/java/ricard/projecte/exoplayer_technicaltest/Framework_ExoPlayer.java
@@ -1,7 +1,9 @@
 package ricard.projecte.exoplayer_technicaltest;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
+import android.widget.TextView;
 
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.MediaItem;
@@ -11,6 +13,10 @@ import com.google.android.exoplayer2.util.MimeTypes;
 import com.google.android.exoplayer2.util.Util;
 
 import java.io.File;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.Temporal;
+import java.util.concurrent.TimeUnit;
 
 public class Framework_ExoPlayer{
 
@@ -18,6 +24,41 @@ public class Framework_ExoPlayer{
         MediaItem mi = MediaItem.fromUri(Uri.parse("http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8"));
 
         return mi;
+    }
+
+    public static void countPause(TextView t, int count){
+        t.setText("");
+        t.setText(String.valueOf(count));
+    }
+
+    public static void countPlay(TextView t, int count){
+        t.setText("");
+        t.setText(String.valueOf(count));
+    }
+
+    public static void countRepeat(TextView t, int count){
+        t.setText("");
+        t.setText(String.valueOf(count));
+    }
+
+    public static void getElapsedTime(Instant start, Instant end, TextView t){
+        long min = 0, sec=0, timelapsed = 0;
+        String time="";
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            Duration timeElapsed = Duration.between(start, end);
+            min = timeElapsed.toMinutes();
+            sec = timeElapsed.toNanos();
+            timelapsed = timeElapsed.toMillis();
+
+            time =  String.format("%02d min, %02d sec",
+                    TimeUnit.MILLISECONDS.toMinutes(timelapsed),
+                    TimeUnit.MILLISECONDS.toSeconds(timelapsed) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(timelapsed))
+            );
+
+            t.setText("");
+            t.setText(time);
+        }
     }
 
 }
--- app/src/main/java/ricard/projecte/exoplayer_technicaltest/MainActivity.java
+++ app/src/main/java/ricard/projecte/exoplayer_technicaltest/MainActivity.java
@@ -1,14 +1,20 @@
 package ricard.projecte.exoplayer_technicaltest;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.exoplayer2.MediaItem;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.analytics.AnalyticsListener;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.extractor.ExtractorsFactory;
 import com.google.android.exoplayer2.source.MediaSource;
@@ -17,47 +23,95 @@ import com.google.android.exoplayer2.upstream.RawResourceDataSource;
 import com.google.android.exoplayer2.util.MimeTypes;
 
 import java.io.File;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.SimpleTimeZone;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
+    SimpleExoPlayer player;
+    PlayerView playview;
+    ImageButton btPausa, btPlay, btRepeat;
+    TextView clickPause, clickPlay, clickRestart, timeElapsed;
+    int contPause = 0, contPlay=0, contRepeat = 0;
+    Instant start, end;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FrameLayout fl = findViewById(R.id.Flayout_player);
 
+        btPausa = findViewById(R.id.exo_pause);
+        btPlay = findViewById(R.id.exo_play);
+        btRepeat = findViewById(R.id.Exo_repeat);
+        clickPause = findViewById(R.id.Tv_PauseClicks);
+        clickPlay = findViewById(R.id.Tv_PlayClicks);
+        clickRestart = findViewById(R.id.Tv_Restart);
+        timeElapsed = findViewById(R.id.Tv_Time);
 
-        PlayerView playview = new PlayerView(this);
-        fl.addView(playview);
 
-        SimpleExoPlayer player = new SimpleExoPlayer.Builder(this).build();
-
+        playview = findViewById(R.id.Pv_exo);
+        player = new SimpleExoPlayer.Builder(this).build();
         playview.setPlayer(player);
-
         MediaItem mi = Framework_ExoPlayer.getMediaItem();
-
-        //player.setMediaItem(getMediaItem());
-
+        player.addAnalyticsListener(new AnalyticsListener() {
+            @Override
+            public void onRenderedFirstFrame(EventTime eventTime, Object output, long renderTimeMs) {
+                Toast.makeText(MainActivity.this, "Rendered the first frame.", Toast.LENGTH_SHORT).show();
+            }
+        });
         player.addMediaItem(mi);
-
         player.prepare();
-
         player.setPlayWhenReady(true);
 
+        Toast.makeText(MainActivity.this, "Welcome to ExoPlayer.", Toast.LENGTH_SHORT).show();
 
 
+        btPausa.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                if (player != null) {
+                    player.setPlayWhenReady(false);
+                }
+                contPause++;
+                Framework_ExoPlayer.countPause(clickPause,contPause);
+                start = Instant.now();
+            }
+        });
 
+        btPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (player != null) {
+                    player.setPlayWhenReady(true);
+                }
+                contPlay++;
+                Framework_ExoPlayer.countPlay(clickPlay,contPlay);
+                end = Instant.now();
+                Framework_ExoPlayer.getElapsedTime(start,end, timeElapsed);
+            }
+        });
 
-
-
+        btRepeat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (player != null) {
+                    player.seekTo(0);
+                }
+                contRepeat++;
+                Framework_ExoPlayer.countRepeat(clickRestart,contRepeat);
+            }
+        });
 
     }
-//C:\Users\riki\Desktop\CFGS DAM\ExoPlayerTechnicalTest\app\src\main\res\video
 
 
+/*
     @Override
     public void onClick(View v) {
 
-    }
+    }*/
 }
